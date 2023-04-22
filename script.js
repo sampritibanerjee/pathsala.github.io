21e1e78420aca74ec6b87b221356bb4374b034f7
sampritibanerjee <88034572+sampritibanerjee@users.noreply.github.com>
2023-04-22 16:45:18 +0530

    pathsala

---
+++ script.js
@@ -0,0 +1,7 @@
+// let menuBtn = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .flex .navbar');
+
+// menuBtn.addEventListener('click', () => {
+//     menuBtn.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// });
